79a87f993fe7f3ac6ba726bbfadfd7c5c5eaa427
Muhammad Tufail Abbasi <159720992+MuhammadTufail786@users.noreply.github.com>
2024-11-07 09:51:50 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var togglebutton = document.getElementById('toggle-Skills');
+var skills = document.getElementById("Skills");
+togglebutton.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
